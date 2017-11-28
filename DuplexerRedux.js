a77c2fe7190fdccb4761d67aa82a00915199f5c6
Gregory <kofmangregory@gmail.com>
2017-11-28 09:13:33 -0500

    Added DuplexerRedux.js

---
+++ DuplexerRedux.js
@@ -0,0 +1,18 @@
+var duplex = require("duplexer2");
+var through = require("through2").obj;
+
+module.exports = function(counter) {
+	var counts = {};
+	var input = through(write, end);
+	return duplex({objectMode: true}, input, counter);
+
+	function write (row, _, next) {
+		counts[row.country] = (counts[row.country] || 0) + 1;
+		next();
+	}
+
+	function end (done) {
+		counter.setCounts(counts);
+		done();
+	}
+};
